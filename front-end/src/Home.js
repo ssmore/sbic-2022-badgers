a6a5828d9ae2c30191fd353013755c2b0521f11d
ssmore <37231737+ssmore@users.noreply.github.com>
2022-12-10 17:21:24 +0800

    create more features

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -4,11 +4,14 @@ import './dexpert.css'
 import expertiseData from "./expertise.json"
 import reviewData from "./reviews.json"
 import {useState,useEffect} from 'react'
+import kudosData from "./kudos.json"
 export const Home = () =>{
 
   const[expertiseList,setExpertiseList] = useState([]);
   const[testimonialList, setTestimonialList] = useState([]);
   const[skillsList, setSkillsList] = useState([]);
+  const[softSkillsList, setSoftSkillsList] = useState([]);
+  const[hardSkillsList, setHardSkillsList] = useState([]);
 
   const [expertiseTitle, setExpertiseTitle] = useState('');
   const [kudosTitle, setKudosTitle] = useState([]);
@@ -17,6 +20,13 @@ export const Home = () =>{
   const [isExpertiseTab, setExpertiseTab] = useState();
   const [isKudosTab, setKudosTab] = useState();
   const [isReviewTab, setReviewTab] = useState();
+
+  const [isKudoOpen, setIsKudoOpen] = useState(false);
+
+  const toggleOpenKudo = () => {
+    setIsKudoOpen(!isKudoOpen)
+    console.log("kudostate"+isKudoOpen)
+  }
 
   async function addTestimonial(testimonialRow){
     //console.log(JSON.stringify(testimonialRow))
@@ -46,8 +56,8 @@ export const Home = () =>{
   }
 
   async function addSkills(firstSkill, secondSkill){
-    console.log(JSON.stringify(firstSkill))
-    console.log(JSON.stringify(secondSkill))
+    //console.log(JSON.stringify(firstSkill))
+    //console.log(JSON.stringify(secondSkill))
     if (secondSkill!=null){
     var element = <div class = "row">
         <div class = "col">
@@ -67,8 +77,8 @@ export const Home = () =>{
               <div class="row">
               <span class="align-middle">
               <div class="progress">
-                <div class="progress-bar w-50" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-              </div>
+              <div class="progress-bar progress-bar-striped" role="progressbar" style={{width:firstSkill.value }}aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
               </span>
               </div>
               </div>
@@ -94,8 +104,8 @@ export const Home = () =>{
                 <div class="row">
                 <span class="align-middle">
                 <div class="progress">
-                  <div class="progress-bar w-50" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
+                <div class="progress-bar progress-bar-striped" role="progressbar" style={{width:secondSkill.value }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+  </div>
 
                 </span>
                 </div>
@@ -129,8 +139,8 @@ export const Home = () =>{
               <div class="row">
               <span class="align-middle">
               <div class="progress">
-                <div class="progress-bar w-50" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-              </div>
+              <div class="progress-bar progress-bar-striped" role="progressbar" style={{width:firstSkill.value }}aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
               </span>
               </div>
               </div>
@@ -160,8 +170,12 @@ export const Home = () =>{
         </div>
         <div class="col-10">
         <div class = "row">
-        <div class="col-4">
-        <h5> {expertiseRow.title} </h5>
+        <div class="col">
+
+        <p class="h5"> {expertiseRow.title}<span class="badge rounded-pill bg-primary" style={{marginLeft:"15px"}}>{expertiseRow.jobCategory}</span> </p>
+
+
+
         </div>
         <div class="col-8 align-items-end" style={{textAlign:"right"}}>
         {expertiseRow.duration}
@@ -177,6 +191,185 @@ export const Home = () =>{
     </div>
     setExpertiseList((prevList) => [...prevList,element])
   }
+
+  async function addSoftSkills(firstSoftSkill,secondSoftSkill,thirdSoftSkill){
+    console.log("first" + firstSoftSkill)
+    console.log("second" + secondSoftSkill)
+    console.log("third" + thirdSoftSkill)
+    var element = ""
+    if(firstSoftSkill!= null && secondSoftSkill!= null && thirdSoftSkill !=null){
+    element = <div class = "row">
+    <div class = "col-sm-4">
+    <div class="card" style={{width:"18rem"}}>
+      <img src={firstSoftSkill.imageUrl}class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">{firstSoftSkill.kudosTitle}</h5>
+        <p class="card-text">{firstSoftSkill.description}</p>
+        <a href="#" class="btn btn-primary">Learn More</a>
+      </div>
+    </div>
+    </div>
+    <div class = "col-sm-4" >
+    <div class="card" style={{width:"18rem"}}>
+      <img src={secondSoftSkill.imageUrl}class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">{secondSoftSkill.kudosTitle}</h5>
+        <p class="card-text">{secondSoftSkill.description}</p>
+        <a href="#" class="btn btn-primary">Learn more</a>
+      </div>
+    </div>
+    </div>
+    <div class = "col-sm-4" >
+    <div class="card" style={{width:"18rem"}}>
+      <img src={thirdSoftSkill.imageUrl} class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">{thirdSoftSkill.kudosTitle}</h5>
+        <p class="card-text">{thirdSoftSkill.description}</p>
+        <a href="#" class="btn btn-primary">Learn more</a>
+      </div>
+    </div>
+    </div>
+    <div class = "medium-break"></div>
+    </div>
+  }else if(firstSoftSkill!=null && secondSoftSkill!=null){
+    element = <div class = "row">
+    <div class = "col-sm-4">
+    <div class="card" style={{width:"18rem"}}>
+    <img src={firstSoftSkill.imageUrl}class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">{firstSoftSkill.kudosTitle}</h5>
+      <p class="card-text">{firstSoftSkill.description}</p>
+      <a href="#" class="btn btn-primary">Learn More</a>
+      </div>
+    </div>
+    </div>
+    <div class = "col-sm-4" >
+    <div class="card" style={{width:"18rem"}}>
+    <img src={secondSoftSkill.imageUrl}class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">{secondSoftSkill.kudosTitle}</h5>
+      <p class="card-text">{secondSoftSkill.description}</p>
+      <a href="#" class="btn btn-primary">Learn more</a>
+      </div>
+    </div>
+    </div>
+    <div class = "medium-break"></div>
+    </div>
+  }else{
+
+    element = <div class = "row">
+    <div class = "col-sm-4">
+    <div class="card" style={{width:"18rem"}}>
+    <img src={firstSoftSkill.imageUrl}class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">{firstSoftSkill.kudosTitle}</h5>
+      <p class="card-text">{firstSoftSkill.description}</p>
+      <a href="#" class="btn btn-primary">Learn More</a>
+      </div>
+    </div>
+    </div>
+
+    <div class = "medium-break"></div>
+    </div>
+
+  }
+
+    setSoftSkillsList((prevList) => [...prevList,element])
+    //add medium break
+    //element = <div class = "medium-break"></div>
+    //setSoftSkillsList((prevList) => [...prevList,element])
+
+  }
+
+  async function addHardSkills(firstSoftSkill,secondSoftSkill,thirdSoftSkill){
+
+    console.log("first" + firstSoftSkill)
+    console.log("second" + secondSoftSkill)
+    console.log("third" + thirdSoftSkill)
+    var element = ""
+    if(firstSoftSkill!= null && secondSoftSkill!= null && thirdSoftSkill !=null){
+    element = <div class = "row">
+    <div class = "col-sm-4">
+    <div class="card" style={{width:"18rem"}}>
+      <img src={firstSoftSkill.imageUrl}class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">{firstSoftSkill.kudosTitle}</h5>
+        <p class="card-text">{firstSoftSkill.description}</p>
+        <a href="#" class="btn btn-primary">Learn More</a>
+      </div>
+    </div>
+    </div>
+    <div class = "col-sm-4" >
+    <div class="card" style={{width:"18rem"}}>
+      <img src={secondSoftSkill.imageUrl}class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">{secondSoftSkill.kudosTitle}</h5>
+        <p class="card-text">{secondSoftSkill.description}</p>
+        <a href="#" class="btn btn-primary">Learn more</a>
+      </div>
+    </div>
+    </div>
+    <div class = "col-sm-4" >
+    <div class="card" style={{width:"18rem"}}>
+      <img src={thirdSoftSkill.imageUrl} class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">{thirdSoftSkill.kudosTitle}</h5>
+        <p class="card-text">{thirdSoftSkill.description}</p>
+        <a href="#" class="btn btn-primary">Learn more</a>
+      </div>
+    </div>
+    </div>
+    <div class = "medium-break"></div>
+    </div>
+  }else if(firstSoftSkill!=null && secondSoftSkill!=null){
+    element = <div class = "row">
+    <div class = "col-sm-4">
+    <div class="card" style={{width:"18rem"}}>
+    <img src={firstSoftSkill.imageUrl}class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">{firstSoftSkill.kudosTitle}</h5>
+      <p class="card-text">{firstSoftSkill.description}</p>
+      <a href="#" class="btn btn-primary">Learn More</a>
+      </div>
+    </div>
+    </div>
+    <div class = "col-sm-4" >
+    <div class="card" style={{width:"18rem"}}>
+    <img src={secondSoftSkill.imageUrl}class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">{secondSoftSkill.kudosTitle}</h5>
+      <p class="card-text">{secondSoftSkill.description}</p>
+      <a href="#" class="btn btn-primary">Learn more</a>
+      </div>
+    </div>
+    </div>
+    <div class = "medium-break"></div>
+    </div>
+  }else{
+
+    element = <div class = "row">
+    <div class = "col-sm-4">
+    <div class="card" style={{width:"18rem"}}>
+    <img src={firstSoftSkill.imageUrl}class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">{firstSoftSkill.kudosTitle}</h5>
+      <p class="card-text">{firstSoftSkill.description}</p>
+      <a href="#" class="btn btn-primary">Learn More</a>
+      </div>
+    </div>
+    <div class = "medium-break"></div>
+    </div>
+
+
+    </div>
+  }
+
+
+
+    setHardSkillsList((prevList) => [...prevList,element])
+
+  }
+
 
   useEffect (()=>{
     console.log(expertiseData)
@@ -208,13 +401,54 @@ export const Home = () =>{
     }
 
 
+    setSoftSkillsList([])
+    setHardSkillsList([])
+    //Arrange kudos
+    var softSkills = []
+    var hardSkills = []
+    for(var i = 0; i<kudosData.length;i++){
+      if(kudosData[i].type==="Soft"){
+        softSkills.push(kudosData[i])
+      }else(
+        hardSkills.push(kudosData[i])
+      )
+    }
+
+    //SoftSkillsSet
+    for(var i = 0; i<softSkills.length;i+=3){
+      var firstSkill = softSkills[i]
+      var secondSkill=null
+      var thirdSkill=null
+      if(i+1 < softSkills.length){
+        secondSkill = softSkills[i+1]
+      }
+      if(i+2 < softSkills.length){
+        thirdSkill = softSkills[i+2]
+      }
+          addSoftSkills(firstSkill,secondSkill,thirdSkill)
+    }
+    //HardSkillsSet
+    for(var i = 0; i<hardSkills.length;i+=3){
+      console.log(JSON.stringify(hardSkills[i]))
+      var firstSkill = hardSkills[i]
+      var secondSkill=null
+      var thirdSkill=null
+      if(i+1 < hardSkills.length){
+        secondSkill = hardSkills[i+1]
+      }
+      if(i+2 < hardSkills.length){
+        thirdSkill = hardSkills[i+2]
+      }
+          addHardSkills(hardSkills[i],secondSkill,thirdSkill)
+    }
+
+
     setExpertiseTitle(<b>Expertise & Skills</b> )
     setExpertiseTab(true)
     setKudosTab(false)
     setKudosTitle('Dexperts Kudos')
     setReviewTab(false)
     setReviewsTitle('Reviews')
-    clickReview()
 
 
   },[])
@@ -270,16 +504,16 @@ export const Home = () =>{
         </ul>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2  d-flex flex-row-reverse justify-content-end mb-md-0">
-          <li><a href="#" class="nav-link px-2 text-secondary">Marketplace</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">My Community</a></li>
+          <li><a href="#" class="nav-link px-2 text-secondary" style={{marginLeft:"70px"}}>Marketplace</a></li>
+          <li><a href="#" class="nav-link px-2 text-white"style={{marginLeft:"120px"}} >My Community</a></li>
         </ul>
 
 
 
         <div class="text-end">
-          <button type="button" class="btn btn-outline-light me-2">Connected</button>
+          <button type="button" class="btn btn-outline-light me-2"  >Connected</button>
         </div>
-        <div class="dropdown text-end">
+        <div class="dropdown text-end" style={{marginLeft:"25px"}}>
           <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
             <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle"/>
           </a>
@@ -357,10 +591,25 @@ export const Home = () =>{
 
 {isKudosTab ? (
 <div class="container">
+<div class = "row align-self-end">
+<div class = "col align-self-end">
+  <button type="button" class="btn btn-primary float-end" onClick={toggleOpenKudo}>Send Kudos</button>
+
+    </div>
+</div>
 <div class = "row">
-    <h5> Kudos </h5>
+    <h5> Hard Skills </h5>
 </div>
 
+
+
+{hardSkillsList}
+<div class="large-break"></div>
+<div class = "row">
+    <h5> Soft Skills </h5>
+</div>
+
+{softSkillsList}
 
 
 
@@ -391,20 +640,72 @@ export const Home = () =>{
   </div>
 
 
-
-
-
-
-
-
-
 ) : (
   <div></div>
 )
 
 }
 
+{isKudoOpen ? (
+  <div class="container">
+  <div class="lock-screen">
+  <div class ="box">
+  <div class = "row">
+  <div class = "col align-self-end">
+  <button type="button" class="btn-close float-end" onClick={toggleOpenKudo}></button>
+  </div>
+  </div>
 
+
+        <h5 class="modal-title"> Send Kudos</h5>
+        <hr/>
+        <form>
+  <div class="mb-3">
+    <label for="recipient" class="form-label">Recipient</label>
+    <input type="email" class="form-control" id="recipientValue" aria-describedby="emailHelp"/>
+  </div>
+
+  <select class="form-select" aria-label="Default select example">
+  <option disabled>Kudos Type</option>
+  <option value="1">Ruby Development</option>
+  <option value="2">Stakeholder Development</option>
+  <option value="3">Software Engineering</option>
+</select>
+
+  <div class="mb-3">
+    <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+
+    <div class="mb-3">
+  <label for="formFileSm" class="form-label">Kudos Image</label>
+  <input class="form-control form-control-sm" id="formFileSm" type="file"/>
+</div>
+  <button type="buttom" onClick={toggleOpenKudo} class="btn btn-primary">Submit</button>
+</form>
+
+  </div>
+  </div>
+  </div>
+
+):(<div></div>)}
+
+<div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        <svg class="bi" width="30" height="24"></svg>
+      </a>
+      <span class="mb-3 mb-md-0 text-muted">&copy; Dexperts</span>
+    </div>
+
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"></svg></a></li>
+      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"></svg></a></li>
+      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"></svg></a></li>
+    </ul>
+  </footer>
+</div>
 
 
   </>
