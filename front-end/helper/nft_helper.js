26445191726890b54343673f817475dc429198ed
Rui Qin Ng <ngruiqin_1994@hotmail.com>
2022-12-30 20:25:27 +0800

    Update nft_helper.js

--- front-end/helper/nft_helper.js
+++ front-end/helper/nft_helper.js
@@ -3,14 +3,13 @@ import { ethers } from "ethers";
 require("dotenv").config()
 const NFT_CONTRACT_ADDRESS = process.env.NFT_CONTRACT_ADDRESS;
 
-const contract = require("");  // TODO: Deploy contract via Remix and get artifact
+const contract = require("../../contracts/artifacts/BadgersNFT.json");  // TODO: Deploy contract via Remix and get artifact
 const abi = contract.abi;
 
-async function awardBadge(signer, recipient, tokenURI) {
+async function awardBadge(abi, signer, recipient, skillName, tokenURI) {
     const nftContract = new ethers.Contract(NFT_CONTRACT_ADDRESS, abi, signer);
-    const txn = await nftContract.mint(recipient, tokenURI);
+    const txn = await nftContract.mint(recipient, skillName, tokenURI);
     const txnReceipt = await txn.wait();
 
-    const event = txnReceipt.events[0];
-    return event["tokenId"];
+    return txnReceipt.status == 1;
 }
