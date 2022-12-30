e8e2477c83bde32a826f3fbb51304f2f6cf20d07
Rui Qin Ng <ngruiqin_1994@hotmail.com>
2022-12-30 20:25:10 +0800

    Update contracts and add abi

--- contracts/BadgersNFT.sol
+++ contracts/BadgersNFT.sol
@@ -4,27 +4,32 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract BadgersNFT is ERC721Enumerable {
+contract BadgersNFT is ERC721Enumerable, ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _idCounter;
 
+    mapping(uint256 => string) public skillNameMap;
+
     constructor() ERC721("BadgersNFT", "BDG") {}
 
-    function mint(address recipient, string tokenURI) public
+    function mint(address recipient, string memory skillName, string memory _tokenURI) public
         returns (uint256)
     {
         _idCounter.increment();
         uint256 tokenId = _idCounter.current();
         
         _safeMint(recipient, tokenId);
-        _setTokenURI(tokenId, tokenURI);
+
+        skillNameMap[tokenId] = skillName;
+        _setTokenURI(tokenId, _tokenURI);
         return tokenId;
     }
 
-    function getTokensOfOwner(address owner) public 
-        returns (uint256[])
+    function getTokensOfOwner(address owner) public view 
+        returns (uint256[] memory)
     {
         uint256 num_tokens = balanceOf(owner);
         require(num_tokens > 0, "NO_TOKEN_IN_POSSESSION");
@@ -36,6 +41,41 @@ contract BadgersNFT is ERC721Enumerable {
         }
         
         return tokenIds;
+    }
+
+    function getSkillName(uint256 tokenId) public view
+        returns (string memory)
+    {
+        return skillNameMap[tokenId];
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
     
 }
--- contracts/BadgersProfile.sol
+++ contracts/BadgersProfile.sol
@@ -7,28 +7,29 @@ contract BadgersProfile {
         string company;
         string position;
         string description;
-        mapping[string => uint256[]] skillToBadgeMap;
+        uint256[] badges;
     }
 
     struct User {
-        string name,
-        Experience[] experiences
+        string name;
+        Experience[] experiences;
     }
     
     mapping(address => User) public userMap;
 
-    function createProfile(string displayName) public
+    function createProfile(string memory displayName) public
     {
-        User memory user = userMap[msg.sender];
-        require(bytes(user) == 0, "USER_EXISTS");
-        
-        userMap[msg.sender] = User(displayName, 0);
+        User storage user = userMap[msg.sender];
+        require(bytes(user.name).length == 0, "USER_EXISTS");
+
+        user.name = displayName;
+        userMap[msg.sender] = user;
     }
 
-    function updateDisplayName(string displayName) public
+    function updateDisplayName(string memory displayName) public
     {
-        User memory user = userMap[msg.sender];
-        require(bytes(user) != 0, "USER_NOT_EXIST");
+        User storage user = userMap[msg.sender];
+        require(bytes(user.name).length != 0, "USER_NOT_EXIST");
         
         user.name = displayName;
         userMap[msg.sender] = user;
@@ -37,83 +38,69 @@ contract BadgersProfile {
     function removeProfile() public
     {
         User memory user = userMap[msg.sender];
-        require(bytes(user) != 0, "USER_NOT_EXIST");
+        require(bytes(user.name).length != 0, "USER_NOT_EXIST");
 
         delete userMap[msg.sender];
     }
 
-    function addExperience(string company, string position, string description) public 
+    function addExperience(string memory company, string memory position, string memory description) public 
     {
-        User memory user = userMap[msg.sender];
-        require(bytes(user) != 0, "USER_NOT_EXIST");
+        User storage user = userMap[msg.sender];
+        require(bytes(user.name).length != 0, "USER_NOT_EXIST");
 
-        Experience memory experience = Experience(company, position, description);
+        Experience memory experience = Experience(company, position, description, new uint256[](0));
         
-        if (bytes(user.experiences) == 0) {
-            // User has no experiences, adding first experience
-            Experience[] memory experiences;
-        } else {
-            Experience[] memory experiences = user.experiences;
-        }
-
+        Experience[] storage experiences = user.experiences;
         experiences.push(experience);
         user.experiences = experiences;
     }
 
-    function updateExperience(uint idx, string company, string position, string description) public 
+    function updateExperience(uint idx, string memory company, string memory position, string memory description) public  
     {
-        User memory user = userMap[msg.sender];
-        require(bytes(user) != 0, "USER_NOT_EXIST");
+        User storage user = userMap[msg.sender];
+        require(bytes(user.name).length != 0, "USER_NOT_EXIST");
 
-        Experience[] memory experiences = user.experiences;
+        Experience[] storage experiences = user.experiences;
         require((idx >= 0) && (idx < experiences.length), "INDEX_OUT_OF_RANGE");
 
-        Experience memory experience = Experience(company, position, description);
+        Experience storage experience = experiences[idx];
+        
+        experience.company = company;
+        experience.position = position;
+        experience.description = description;
+
         experiences[idx] = experience;
         user.experiences = experiences;
     }
 
     function removeExperience(uint idx) public 
     {
-        User memory user = userMap[msg.sender];
-        require(bytes(user) != 0, "USER_NOT_EXIST");
+        User storage user = userMap[msg.sender];
+        require(bytes(user.name).length != 0, "USER_NOT_EXIST");
 
-        Experience[] memory experiences = user.experiences;
+        Experience[] storage experiences = user.experiences;
         require((idx >= 0) && (idx < experiences.length), "INDEX_OUT_OF_RANGE");
 
         experiences[idx] = experiences[experiences.length-1];  // Order not preserved
         experiences.pop();
     }
 
-    function addBadgeToExperience(address recipientAddress, uint expIdx, string skillName, int256 tokenId) public 
+    function addBadgeToExperience(address recipientAddress, uint expIdx, uint256 tokenId) public 
     {
         User memory sender = userMap[msg.sender];
-        require(bytes(sender) != 0, "SENDER_NOT_REGISTERED");
+        require(bytes(sender.name).length != 0, "SENDER_NOT_REGISTERED");
 
-        User memory recipient = userMap[recipientAddress];
-        require(bytes(recipient) != 0, "RECIPIENT_NOT_EXIST");
+        User storage recipient = userMap[recipientAddress];
+        require(bytes(recipient.name).length != 0, "RECIPIENT_NOT_EXIST");
 
-        Experience[] memory experiences = recipient.experiences;
+        Experience[] storage experiences = recipient.experiences;
         require((expIdx >= 0) && (expIdx < experiences.length), "INDEX_OUT_OF_RANGE");
 
-        Experience memory experience = experiences[expIdx];
-        if (bytes(experience.skillToBadgeMap) == 0) {
-            // User has no skill in this experience
-            mapping[string => uint256[]] memory skillToBadgeMap;
-        } else {
-            mapping[string => uint256[]] memory skillToBadgeMap = experience.skillToBadgeMap;
-        }
-
-        if (bytes(skillToBadgeMap[skillName]) == 0) {
-            // User has no badge in this experience
-            uint256[] memory badges;
-        } else {
-            uint256[] memory badges = skillToBadgeMap[skillName];
-        }
+        Experience storage experience = experiences[expIdx];
+        uint256[] storage badges = experience.badges;
 
         badges.push(tokenId);
-        skillToBadgeMap[skillName] = badges;
-        experience.skillToBadgeMap = skillToBadgeMap;
+        experience.badges = badges;
         experiences[expIdx] = experience;
         recipient.experiences = experiences;
     }
