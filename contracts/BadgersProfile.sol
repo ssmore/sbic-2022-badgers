6608189cacf827e980fdd2430df86ce3793a8141
Rui Qin Ng <36036788+ngrq123@users.noreply.github.com>
2023-01-04 23:18:50 +0800

    Update BadgersProfile

--- contracts/BadgersProfile.sol
+++ contracts/BadgersProfile.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.8.0;
 
 contract BadgersProfile {
 
+    struct Review {
+        address reviewer;
+        string image;
+        string description;
+    }
+
     struct Experience {
         string company;
         string title;
@@ -18,35 +24,19 @@ contract BadgersProfile {
         string name;
         string image;
         Experience[] experiences;
+        Review[] reviews;
     }
     
     mapping(address => User) public userMap;
 
-    function createProfile(string memory displayName, string memory imageURL) public
+    function createProfile(string memory displayName, string memory imageURI) public
     {
         User storage user = userMap[msg.sender];
         require(bytes(user.name).length == 0, "USER_EXISTS");
 
         user.name = displayName;
-        user.image = imageURL
+        user.image = imageURI;
         userMap[msg.sender] = user;
-    }
-
-    function updateDisplayName(string memory displayName) public
-    {
-        User storage user = userMap[msg.sender];
-        require(bytes(user.name).length != 0, "USER_NOT_EXIST");
-        
-        user.name = displayName;
-        userMap[msg.sender] = user;
-    }
-
-    function removeProfile() public
-    {
-        User memory user = userMap[msg.sender];
-        require(bytes(user.name).length != 0, "USER_NOT_EXIST");
-
-        delete userMap[msg.sender];
     }
 
     function addExperience(string memory company, string memory title, uint256 startDate, 
@@ -63,7 +53,7 @@ contract BadgersProfile {
         user.experiences = experiences;
     }
 
-    function getExperiences(address userAddress) public view
+    function getExperiencesOfUser(address userAddress) public view
         returns (string[] memory companies, string[] memory titles, uint256[] memory startDates, 
                     uint256[] memory endDates, string[] memory categories, string[] memory images, 
                     string[] memory descriptions)
@@ -76,7 +66,7 @@ contract BadgersProfile {
         string[] memory _titles = new string[](arrLength);
         uint256[] memory _startDates = new uint256[](arrLength);
         uint256[] memory _endDates = new uint256[](arrLength);
-        string[] memory _categories= new string[](arrLength);
+        string[] memory _categories = new string[](arrLength);
         string[] memory _images = new string[](arrLength);
         string[] memory _descriptions = new string[](arrLength);
 
@@ -125,133 +115,38 @@ contract BadgersProfile {
 
         return user.experiences[expIdx].badges;
     }
-    
-}// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
 
-contract BadgersProfile {
-
-    struct Experience {
-        string company;
-        string title;
-        uint256 startDate;
-        uint256 endDate;
-        string category;
-        uint256[] badges;
-        string image;
-        string description;
-    }
-
-    struct User {
-        string name;
-        string image;
-        Experience[] experiences;
-    }
-    
-    mapping(address => User) public userMap;
-
-    function createProfile(string memory displayName, string memory imageURL) public
+    function addReview(address recipient, string memory image, string memory description) public 
     {
-        User storage user = userMap[msg.sender];
-        require(bytes(user.name).length == 0, "USER_EXISTS");
-
-        user.name = displayName;
-        user.image = imageURL
-        userMap[msg.sender] = user;
-    }
-
-    function updateDisplayName(string memory displayName) public
-    {
-        User storage user = userMap[msg.sender];
+        User storage user = userMap[recipient];
         require(bytes(user.name).length != 0, "USER_NOT_EXIST");
+
+        Review memory review = Review(msg.sender, image, description);
         
-        user.name = displayName;
-        userMap[msg.sender] = user;
+        Review[] storage reviews = user.reviews;
+        reviews.push(review);
+        user.reviews = reviews;
     }
 
-    function removeProfile() public
-    {
-        User memory user = userMap[msg.sender];
-        require(bytes(user.name).length != 0, "USER_NOT_EXIST");
-
-        delete userMap[msg.sender];
-    }
-
-    function addExperience(string memory company, string memory title, uint256 startDate, 
-                            uint256 endDate, string memory category, string memory image, 
-                            string memory description) public 
-    {
-        User storage user = userMap[msg.sender];
-        require(bytes(user.name).length != 0, "USER_NOT_EXIST");
-
-        Experience memory experience = Experience(company, title, startDate, endDate, category, new uint256[](0), image, description);
-        
-        Experience[] storage experiences = user.experiences;
-        experiences.push(experience);
-        user.experiences = experiences;
-    }
-
-    function getExperiences(address userAddress) public view
-        returns (string[] memory companies, string[] memory titles, uint256[] memory startDates, 
-                    uint256[] memory endDates, string[] memory categories, string[] memory images, 
-                    string[] memory descriptions)
-    {
-        User memory user = userMap[userAddress];
-        require(bytes(user.name).length != 0, "USER_NOT_EXIST");
-
-        uint256 arrLength = user.experiences.length;
-        string[] memory _companies = new string[](arrLength);
-        string[] memory _titles = new string[](arrLength);
-        uint256[] memory _startDates = new uint256[](arrLength);
-        uint256[] memory _endDates = new uint256[](arrLength);
-        string[] memory _categories= new string[](arrLength);
-        string[] memory _images = new string[](arrLength);
-        string[] memory _descriptions = new string[](arrLength);
-
-        for (uint idx = 0; idx < user.experiences.length; idx++) {
-            Experience memory experience = user.experiences[idx];
-            _companies[idx] = experience.company;
-            _titles[idx] = experience.title;
-            _startDates[idx] = experience.startDate;
-            _endDates[idx] = experience.endDate;
-            _categories[idx] = experience.category;
-            _images[idx] = experience.image;
-            _descriptions[idx] = experience.description;
-        }
-
-        return (_companies, _titles, _startDates, _endDates, _categories, _images, _descriptions);
-    }
-
-    function addBadgeToExperience(address recipientAddress, uint expIdx, uint256 tokenId) public 
-    {
-        User memory sender = userMap[msg.sender];
-        require(bytes(sender.name).length != 0, "SENDER_NOT_REGISTERED");
-
-        User storage recipient = userMap[recipientAddress];
-        require(bytes(recipient.name).length != 0, "RECIPIENT_NOT_EXIST");
-
-        Experience[] storage experiences = recipient.experiences;
-        require((expIdx >= 0) && (expIdx < experiences.length), "INDEX_OUT_OF_RANGE");
-
-        Experience storage experience = experiences[expIdx];
-        uint256[] storage badges = experience.badges;
-
-        badges.push(tokenId);
-        experience.badges = badges;
-        experiences[expIdx] = experience;
-        recipient.experiences = experiences;
-    }
-
-    function getBadgesOfExperience(address userAddress, uint256 expIdx) public view
-        returns (uint256[] memory tokenIds)
+    function getReviewsOfUser(address userAddress) public view
+        returns (address[] memory reviewers, string[] memory images, string[] memory descriptions)
     {
         User storage user = userMap[userAddress];
         require(bytes(user.name).length != 0, "USER_NOT_EXIST");
 
-        Experience[] storage experiences = user.experiences;
-        require((expIdx >= 0) && (expIdx < experiences.length), "INDEX_OUT_OF_RANGE");
+        uint256 arrLength = user.reviews.length;
+        address[] memory _reviewers = new address[](arrLength);
+        string[] memory _images = new string[](arrLength);
+        string[] memory _descriptions = new string[](arrLength);
 
-        return user.experiences[expIdx].badges;
+        for (uint idx = 0; idx < user.reviews.length; idx++) {
+            Review memory review = user.reviews[idx];
+            _reviewers[idx] = review.reviewer;
+            _images[idx] = review.image;
+            _descriptions[idx] = review.description;
+        }
+
+        return (_reviewers, _images, _descriptions);
     }
     
 }
--- front-end/helper/profile_helper.js
+++ front-end/helper/profile_helper.js
@@ -6,11 +6,88 @@ const PROFILE_CONTRACT_ADDRESS = process.env.PROFILE_CONTRACT_ADDRESS;
 const contract = require("../../contracts/artifacts/BadgersProfile.json");  // TODO: Deploy contract via Remix and get artifact
 const abi = contract.abi;
 
-async function createProfile(abi, signer, displayName) {
-    const profileContract = new ethers.Contract(PROFILE_CONTRACT_ADDRESS, abi, signer);
+const signer ; // TODO
 
-    const txn = await profileContract.createProfile(displayName);
+async function getContract() {
+    const profileContract = new ethers.Contract(PROFILE_CONTRACT_ADDRESS, abi, signer);
+    return profileContract;
+}
+
+async function createProfile(contract, displayName, image) {
+    const txn = await contract.createProfile(displayName, image);
     const txnReceipt = await txn.wait();
     
     return txnReceipt.status == 1;
+}
+
+async function addExperience(contract, company, title, startDate, endDate, category, image, description) {
+    const txn = await contract.addExperience(company, title, startDate, endDate, category, image, description);
+    const txnReceipt = await txn.wait();
+    
+    return txnReceipt.status == 1;
+}
+
+async function getExperiencesOfUser(contract, user) {
+    const details = await contract.getExperiencesOfUser(user);
+
+    companies = details[0];
+    titles = details[1];
+    startDates = details[2];
+    endDates = details[3];
+    categories = details[4];
+    images = details[5];
+    descriptions = details[6];
+
+    let experiences = [];
+    for (let idx = 0; idx < companies.length; idx++) {
+        let badges = await contract.getBadgesOfExperience(user, idx);
+        badges = badges.map(b => parseInt(b, 10));
+        badges = await Promise.all(badges.map(async (b) => await getTokenDetails(await getNFTContract(), b)));
+
+        let experience = {
+            "company": companies[idx],
+            "title": titles[idx],
+            "startDate": (new Date (parseInt(startDates[idx], 10))).toISOString(),
+            "endDate": (new Date (parseInt(endDates[idx], 10))).toISOString(),
+            "category": categories[idx],
+            "image": images[idx],
+            "description": descriptions[idx],
+            "badges": badges
+        };
+        experiences.push(experience);
+    }
+    return experiences;
+}
+
+async function addBadgeToExperience(contract, user, expIdx, tokenId) {
+    const txn = await contract.addBadgeToExperience(user, expIdx, tokenId);
+    const txnReceipt = await txn.wait();
+    
+    return txnReceipt.status == 1;
+}
+
+async function addReview(contract, recipient, image, desc) {
+    const txn = await contract.addReview(recipient, image, desc);
+    const txnReceipt = await txn.wait();
+    
+    return txnReceipt.status == 1;
+}
+
+async function getReviewsOfUser(contract, user) {
+    const details = await contract.getReviewsOfUser(user);
+
+    reviewers = details[0];
+    images = details[1];
+    descriptions = details[2];
+
+    let reviews = [];
+    for (let idx = 0; idx < reviewers.length; idx++) {
+        let review = {
+            "reviewer": reviewers[idx],
+            "image": images[idx],
+            "description": descriptions[idx]
+        };
+        reviews.push(review);
+    }
+    return reviews;
 }
